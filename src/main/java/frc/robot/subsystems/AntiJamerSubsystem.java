d2b83f62245c983325e19b77a7a44ebcc9f7eec2
programming07 <2491nomythic@gmail.com>
2025-05-06 20:06:58 -0500

    started updating to 2025 code

--- src/main/java/frc/robot/subsystems/AntiJamerSubsystem.java
+++ src/main/java/frc/robot/subsystems/AntiJamerSubsystem.java
@@ -4,10 +4,13 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -16,18 +19,24 @@ import frc.robot.Constants;
 
 public class AntiJamerSubsystem extends SubsystemBase {
   	
-	CANSparkMax antiJamMotor;
-	SparkPIDController antiJamController;
+	SparkMax antiJamMotor;
+	SparkMaxConfig antiJamConfig;
+  
 
 	public AntiJamerSubsystem() {
-		antiJamMotor = new CANSparkMax(Constants.AntiJamer.AntiJamerMotorID, MotorType.kBrushless);
-    antiJamController = antiJamMotor.getPIDController();
+		antiJamMotor = new SparkMax(Constants.AntiJamer.AntiJamerMotorID, MotorType.kBrushless);
+    antiJamConfig.apply(new ClosedLoopConfig().pidf(
+      Constants.AntiJamer.Antijamer_kP,
+      Constants.AntiJamer.Antijamer_kI,
+      Constants.AntiJamer.Antijamer_kD,
+      Constants.AntiJamer.Antijamer_kFF));
     antiJamMotor.restoreFactoryDefaults();
-    antiJamController.setP(Constants.AntiJamer.Antijamer_kP);
-    antiJamController.setI(Constants.AntiJamer.Antijamer_kI);
-    antiJamController.setD(Constants.AntiJamer.Antijamer_kD);
-    antiJamController.setFF(Constants.AntiJamer.Antijamer_kFF);
-    antiJamController = antiJamMotor.getPIDController();
+    antiJamConfig.
+    antiJamConfig.s(Constants.AntiJamer.Antijamer_kP);
+    antiJamConfig.setI(Constants.AntiJamer.Antijamer_kI);
+    antiJamConfig.setD(Constants.AntiJamer.Antijamer_kD);
+    antiJamConfig.setFF(Constants.AntiJamer.Antijamer_kFF);
+    antiJamConfig = antiJamMotor.getPIDController();
     antiJamMotor.setIdleMode(IdleMode.kCoast);
     antiJamMotor.setSmartCurrentLimit(25, 40, 1000);
     antiJamMotor.getEncoder().setPositionConversionFactor(1);
@@ -43,7 +52,7 @@ public class AntiJamerSubsystem extends SubsystemBase {
 		SmartDashboard.putNumber("AntiJamer Speed", 0);
 	}
   public void setVelocity(double antiJamerVelocity) {
-    antiJamController.setReference(antiJamerVelocity, CANSparkMax.ControlType.kVelocity);
+    antiJamConfig.setReference(antiJamerVelocity, CANSparkMax.ControlType.kVelocity);
   }
   @Override
   public void periodic() {
